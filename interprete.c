600cfe7c4236692cc445424901b1b7e3940fbc9f
Pomarola <lpoma2000@gmail.com>
2020-05-29 19:27:49 -0300

    Interprete

---
+++ interprete.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char entrada[50], comando[10];
+    int salir = 0;
+    double inicio, final;
+    printf("Ingrese i para insertar, e para eliminar, ? para intersecar, dfs, bfs, salir \n");
+    do{
+        gets(entrada);
+        sscanf(entrada, "%s [%lf,%lf] \n", comando, &inicio, &final);
+
+        if (strcmp(comando,"i") == 0)
+            printf("insertar \n");
+        else if (strcmp(comando,"e") == 0)
+            printf("eliminar \n");
+        else if (strcmp(comando,"?") == 0)
+            printf("intersecar \n");
+        else if (strcmp(comando,"dfs") == 0)
+            printf("dfs \n");
+        else if (strcmp(comando,"bfs") == 0)
+            printf("bfs \n");
+        else if (strcmp(comando,"salir") == 0)
+            salir = 1;
+        else
+            printf("comando no reconocido \n");
+        
+
+        printf("entrada: %s\n",entrada);
+        printf("comando: %s\n",comando);
+        printf("inicio: %lf\n",inicio);
+        printf("final: %lf\n",final);
+    } while (salir == 0);
+
+
+    return 0;
+}
